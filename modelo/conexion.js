3249041fc0de55ea52077674d320be0068c25275
Alfredo Utria <alfredoutria@hotmail.com>
2023-05-22 14:08:03 -0500

    actualizando el proyecto

--- modelo/conexion.js
+++ modelo/conexion.js
@@ -4,6 +4,7 @@ mongoose.connect(process.env.MONGODB_URI,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
 }).then(()=>{
+    console.log("base de datos iniciada");
 }).catch(err =>{
     console.log(err)
 })
